949c5dc80fb5d5d308b7e2bd3a13385a6d1a1602
Iryna Prus <prusirina12@gmail.com>
2025-05-09 18:14:40 +0300

    update task-2-3

--- js/task-2.js
+++ js/task-2.js
@@ -9,7 +9,9 @@ class Storage {
   addItem(newItem) {
     this.#items.push(newItem);
   }
-  removeItem(itemToRemove) {}
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
